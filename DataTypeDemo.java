e78c21ff50d9a784c1ee6bee19b1e0f31e6ecbe1
杜肖肖 <38879281+shalldou@users.noreply.github.com>
2018-05-22 10:42:01 +0800

    Add files via upload

---
+++ DataTypeDemo.java
@@ -0,0 +1,87 @@
+package day02;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+
+		int a = 25;
+		long b = a;
+		int c = (int)b;
+		long d = 10000000000L;
+		int e = (int)d;
+		System.out.println(e);
+		
+		
+		
+		/*
+		
+		int a = 25;
+		long b = a;
+		int c = (int)b;
+		long d = 10000000000L;
+		int e = (int) d;
+		System.out.println(e);
+		double f = 14.986;
+		int g = (int)f;
+		System.out.println(g);
+		short b1 = 5;
+		short b2 = 8;
+		short b3 = (short)(b1+b2);
+		System.out.println(2+2);
+		System.out.println(2+'2');
+		System.out.println('2'+'2');
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+/*
+	int a = 25;
+	//int b = 10000000000;
+	System.out.println(5/3);//1
+	System.out.println(3/5);//0
+	System.out.println(3.0/5);//0.6
+	int c = 2147483647;
+	c = c + 1;
+	System.out.println(c);//-2147483648
+	long d = 1000000000;
+	long e = 1000000000*2*10L;
+	System.out.println(e);//2000000000
+	long f = 1000000000*3*10L;
+	System.out.println(f);//
+	long g = 1000000000L*3*10;
+	System.out.println(g);//3000000000
+	long h=System.currentTimeMillis();
+	System.out.println(h);//
+	/*
+	double pi  = 3.14;
+	double i = 3.0,j = 2.9;
+	System.out.println(i-j);//
+	boolean b1 = true;
+	boolean b2 = false;
+	//boolean b3 = 250;
+	char c1 = 'ÄÐ';
+	char c2 = 'm';
+	char c3 = '5';
+	System.out.println(c3);
+	char c4 = ' ';
+	//char c5 = '';
+	//char c6 = 'ÄÐÐÔ';
+	char c7 = 97;
+	System.out.println(c7);//a
+	char c8 = '\'';
+	System.out.println(c8);//'
+	*/
+
+	}
+
+}
