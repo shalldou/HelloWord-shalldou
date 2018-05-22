b89e0ea6acb8415242a405957644eb1294e1e72f
杜肖肖 <38879281+shalldou@users.noreply.github.com>
2018-05-22 10:43:12 +0800

    Add files via upload

---
+++ VarDemo.java
@@ -0,0 +1,33 @@
+package day02;
+
+public class VarDemo {
+
+	public static void main(String[] args) {
+	//1
+		int a ;
+	int b,c;
+	//2
+	int d = 5;
+	int e;
+	e = 5;
+	//3
+	int f = 6;
+	int g = f + 10;
+	System.out.println(f);
+	f = f + 10;
+	System.out.println(f); 
+	//int h = 25.47;
+	//System.out.println(i);
+	int i;
+	//System.out.println(i);
+	//4
+	int  x = 8;
+	//int  y = 3.15;
+	//int 3s = 5;
+	int f_ = 25;
+	
+	
+
+	}
+
+}
